517fe62aee69996f4e7be7674a1595c4b1e01db4
berkslv <berkslv@gmail.com>
2022-08-23 12:53:48 +0300

    Added MVC client and removed react

---
+++ client/Client/Controllers/FeedController.cs
@@ -0,0 +1,31 @@
+﻿using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using Client.Models;
+
+namespace Client.Controllers;
+
+public class FeedController : Controller
+{
+    private readonly ILogger<FeedController> _logger;
+
+    public FeedController(ILogger<FeedController> logger)
+    {
+        _logger = logger;
+    }
+
+    public IActionResult Index()
+    {
+        return View();
+    }
+
+    public IActionResult Privacy()
+    {
+        return View();
+    }
+
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Error()
+    {
+        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+    }
+}
